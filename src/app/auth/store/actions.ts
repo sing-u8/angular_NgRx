95397d3d8597b6885f868fe14e24a285b56970cc
sing-u8 <singyu13@gmail.com>
2021-11-10 23:21:50 +0900

    create auth interfaces

--- src/app/auth/store/actions.ts
+++ src/app/auth/store/actions.ts
@@ -1,7 +1,9 @@
 import { createAction, props } from '@ngrx/store'
 import { ActionTypes } from 'src/app/auth/store/actionTypes'
 
+import { RegisterRequestInterface } from '@auth/types/registerRequest.interface'
+
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{ username: string; password: string; email: string }>()
+  props<{ request: RegisterRequestInterface }>()
 )
